ffb1cb5b4d4f840321aad9dfffb7b18be1006458
Abdi Omari <74247785+abdiomari@users.noreply.github.com>
2022-07-19 23:08:09 +0300

    first commit
    coffee machine now in OOP implementation still the same coffee

---
+++ coffee machine in OOP/main.py
@@ -0,0 +1,13 @@
+# 1. print reports
+# 2. Check resources sufficient?
+# 3. Process coins.
+# 4. Check transaction successful?
+# 5. Make coffee
+
+
+from coffee_maker import CoffeeMaker
+from menu import Menu, MenuItem
+from money_machine import MoneyMachine
+
+
+money_machine= 
