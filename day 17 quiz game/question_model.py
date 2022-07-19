3f7fecea801ff86fbfd208664b430e88c919e360
Abdi Omari <74247785+abdiomari@users.noreply.github.com>
2022-07-19 23:10:05 +0300

    first commit
    Day 17 quiz game

---
+++ day 17 quiz game/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+# new_q = Question("some question", "some answer")
+# print(Question.answer)
